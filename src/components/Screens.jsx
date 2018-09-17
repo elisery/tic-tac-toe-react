89f132af345aa389f3f9e57ceae6276191eeead9
Elise Yohm <elisery@hotmail.com>
2018-09-16 20:02:21 -0700

    Fix functionality for TokenChoice component

--- src/components/Screens.jsx
+++ src/components/Screens.jsx
@@ -17,35 +17,28 @@ class Screens extends Component {
   }
 
   playerChoice(option) {
-    // console.log(option);
-    let test = this.state.gameType;
     this.setState({ gameType: option });
-    console.log('what', this.state.gameType);
   }
   /*
   1. if no player choice set render player choice screen ChoosePlayer
   2. next if no x or o choice set render token choice screen TokenChoice
   3. else render PlayBoard
-
-   - OPTION: 
-   1. make a "screens" component - and call it after <Settings />
-   2. within settings - conditionally render chooseplayer, tokenchoice or playboard
   */
 
   tokenChoice(option) {
     let secondOption = ''
     option === 'x-token' ? secondOption = 'o-token' : secondOption = 'x-token';
 
-    this.state.gameType === '2-player' ? 
-      this.setState({ playerTwoToken: secondOption }) : 
+    if (this.state.gameType === '2-player') {
+      this.setState({ playerTwoToken: secondOption });
+    } else {
       this.setState({ computerToken: secondOption })
-    console.log('player one', this.state.playerOneToken);
-    console.log('player two', this.state.playerOneToken);
-    console.log('computer', this.state.computerToken);
+    }
+    this.setState({ playerOneToken: option })
   }
 
   render() {
-    const { gameType, playerOneToken, playerTwoToken } = this.state;
+    const { gameType, playerOneToken } = this.state;
     if (!gameType) {
       return(
         <ChoosePlayer onChoiceClick={this.playerChoice} />
